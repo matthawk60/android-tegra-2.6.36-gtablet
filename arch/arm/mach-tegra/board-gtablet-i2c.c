019edf71ff41583e7ae056c4ccf883e962edd900
Icewyng <boue27@gmail.com>
2011-08-20 18:28:43 -0300

    Reformatted i2c2 bariables

--- arch/arm/mach-tegra/board-gtablet-i2c.c
+++ arch/arm/mach-tegra/board-gtablet-i2c.c
@@ -1,7 +1,7 @@
 /*
  * arch/arm/mach-tegra/board-gtablet-i2c.c
  *
- * Copyright (C) 2011 Eduardo José Tagle <ejtagle@tutopia.com>
+ * Copyright (C) 2011 Eduardo Josï¿½ Tagle <ejtagle@tutopia.com>
  * Copyright (C) 2011 Jens Andersen <jens.andersen@gmail.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -42,32 +42,20 @@ static struct tegra_i2c_platform_data gtablet_i2c1_platform_data = {
 	.bus_clk_rate	= { 100000, 0 },
 };
 
-static const struct tegra_pingroup_config i2c2_ddc = {
-	.pingroup	= TEGRA_PINGROUP_DDC,
-	.func		= TEGRA_MUX_I2C2,
-};
-
-static const struct tegra_pingroup_config i2c2_gen2 = {
-	.pingroup	= TEGRA_PINGROUP_PTA,
-	.func		= TEGRA_MUX_I2C2,
-};
-
 static struct tegra_i2c_platform_data gtablet_i2c2_platform_data = {
 	.adapter_nr	= 1,
-	.bus_count	= 2,
-	.bus_clk_rate	= { 100000, 100000 },
-	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
-	.bus_mux_len	= { 1, 1 },
+	.bus_count	= 1,
+	.bus_clk_rate	= { 100000, 0 },
 };
 
 static struct tegra_i2c_platform_data gtablet_i2c3_platform_data = {
-	.adapter_nr	= 3,
+	.adapter_nr	= 2,
 	.bus_count	= 1,
 	.bus_clk_rate	= { 100000, 0 },
 };
 
 static struct tegra_i2c_platform_data gtablet_dvc_platform_data = {
-	.adapter_nr	= 4,
+	.adapter_nr	= 3,
 	.bus_count	= 1,
 	.bus_clk_rate	= { 100000, 0 },
 	.is_dvc		= true,
